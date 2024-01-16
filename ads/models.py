99dddaf280a6af0f752ba427a90a66e141aba09e
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-16 13:11:35 -0500

    feat: add advertisement and comment models

--- ads/models.py
+++ ads/models.py
@@ -1,1 +1,29 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Advertisement(models.Model):
+    title = models.CharField(
+        max_length=200,
+    )
+    description = models.TextField()
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    publication_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    published = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["publication_date"]
+
+
+class Comment(models.Model):
+    ad = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    rating = models.PositiveSmallIntegerField()
