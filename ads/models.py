d1398b6459205440145f6f11f1d8bb02d6bfb16a
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-16 21:18:49 -0500

    feat: str method for category object

--- ads/models.py
+++ ads/models.py
@@ -1,10 +1,12 @@
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return str(self.name)
 
 
 class Advertisement(models.Model):
