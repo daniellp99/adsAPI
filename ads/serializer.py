e70fdfc80bdc8eaefbbb59735de778f9b4c7ef88
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-16 21:20:04 -0500

    feat: model serializers for users category and advertisement

---
+++ ads/serializer.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from ads.models import Advertisement, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    category = CategorySerializer().read_only
+    owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = Advertisement
+        fields = ["id", "title", "description", "category", "owner"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    advertisements = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Advertisement.objects.all()
+    )
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "advertisements"]
