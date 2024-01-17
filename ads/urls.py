57dc26e759251840e8680e3ace69467067b12655
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-16 21:21:46 -0500

    feat: api endpoint for users categories and advertisement

---
+++ ads/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from ads import views
+
+urlpatterns = [
+    path("user/", views.UserList.as_view()),
+    path("user/<int:pk>/", views.UserDetail.as_view()),
+    path("category/", views.CategoryList.as_view()),
+    path("category/<int:pk>/", views.CategoryList.as_view()),
+    path("advertisement/", views.AdvertisementList.as_view()),
+    path("advertisement/<int:pk>/", views.AdvertisementDetail.as_view()),
+]
